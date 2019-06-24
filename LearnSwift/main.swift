d63fe284bb255694fdaa97351e9b2d3c1e43370a
KevinTamCS <me@kevintam.dev>
2019-06-23 21:49:26 -0400

    Initial Commit

---
+++ LearnSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LearnSwift
+//
+//  Created by Kevin Tam on 6/23/19.
+//  Copyright © 2019 Kevin Tam. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
